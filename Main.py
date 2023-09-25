04c4d377d8436d6ed1c441c21b5ba60996482d61
Dalton Wright <drwright6@mail.fhsu.edu>
2023-09-25 18:55:28 -0500

    Listed the rest of the Requirements.txt file and commented out first attempts

--- Main.py
+++ Main.py
@@ -3,21 +3,18 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 import numpy as np
 
-# Saves the Spotify data as a Pandas frame
+# Saves the Country data as a Pandas frame
 data = pd.read_csv('cost_of_living.csv')
 
 # This limits the results down to just the country name and their cost of living score. Failed Attempt
-USA = data.loc[data['Countries'] == 'USA', 'Countries']
-USA_Cost = data.loc[data['Countries'] == 'USA', 'Cost of living, 2017']
-UK = data.loc[data['Countries'] == 'UK', ['Countries', 'Cost of living, 2017']]
-Canada = data.loc[data['Countries'] == 'Canada', ['Countries', 'Cost of living, 2017']]
-Japan = data.loc[data['Countries'] == 'Japan', ['Countries', 'Cost of living, 2017']]
-Germany = data.loc[data['Countries'] == 'Germany', ['Countries', 'Cost of living, 2017']]
-Russia = data.loc[data['Countries'] == 'Russia', ['Countries', 'Cost of living, 2017']]
-China = data.loc[data['Countries'] == 'China', ['Countries', 'Cost of living, 2017']]
-
-# Testing purposes
-All = [USA, UK, Canada, Japan, Germany, Russia, China]
+# USA = data.loc[data['Countries'] == 'USA', 'Countries']
+# USA_Cost = data.loc[data['Countries'] == 'USA', 'Cost of living, 2017']
+# UK = data.loc[data['Countries'] == 'UK', ['Countries', 'Cost of living, 2017']]
+# Canada = data.loc[data['Countries'] == 'Canada', ['Countries', 'Cost of living, 2017']]
+# Japan = data.loc[data['Countries'] == 'Japan', ['Countries', 'Cost of living, 2017']]
+# Germany = data.loc[data['Countries'] == 'Germany', ['Countries', 'Cost of living, 2017']]
+# Russia = data.loc[data['Countries'] == 'Russia', ['Countries', 'Cost of living, 2017']]
+# China = data.loc[data['Countries'] == 'China', ['Countries', 'Cost of living, 2017']]
 
 def getCountry(countries):
     # Pulls the country for the X axis
@@ -70,11 +67,26 @@ def getCost(COL):
 
 def printGraph(Country):
 
+    # X axis
     countryNames = np.array(getCountry())
 
-    COL =
+    # Y axis
+    COL = np.array(getCost())
 
-    plt.plot(countryNames, )
+    # creates the chart
+    plt.plot(countryNames, COL)
+
+    # labels for the graph
+    plt.xlabel('Country')
+    plt.ylabel('Cost of Living')
+    plt.title('Cost of Living Between Countries')
+
+    # names and saves the chart as a png file
+    savefile = "charts/" + countryNames + ".png"
+    plt.savefig(savefile)
+
+    # prints the graph
+    plt.show()
 
 # Start of program
 
@@ -86,4 +98,6 @@ except FileExistsError:
     pass
 
 for name in getNames():
-    getCountry(Country)
+    getCountry()
+    getCost()
+    printGraph()
